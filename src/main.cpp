5716b919884a2f22fa015b327f9a4f98091aeb3f
shadowme2you <nahombeletew01@gmail.com>
2026-01-03 01:36:09 -0800

    configuration for linux environments such as wsl

---
+++ src/main.cpp
@@ -0,0 +1,7 @@
+#include <iostream>// means include input output stream(stream is a sequence of data)
+
+int main()// the int is used to indicate that the function returnss a an integer value which in our case is 0
+{
+    std::cout<<"hello world";
+    return 0; // 0 indicates that the program executed successfully
+}
